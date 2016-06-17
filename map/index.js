c315f43d9933c9b48dd885abd3a0d3e6e73b4347
Arun Ganesh <arun.planemad@gmail.com>
2016-06-17 20:55:58 +0530

    Remove unused code

--- map/index.js
+++ map/index.js
@@ -2,10 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kzt
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/planemad/cip3y0ycc000jcjnk7bj3abvt', //stylesheet location
-    // center: [77.64, 12.98], // starting position
-    // zoom: 16, // starting zoom
-    hash: true,
-    attributionControl: false
+    hash: true
 });
 
 map.addControl(new mapboxgl.Navigation());
@@ -22,8 +19,6 @@ map.on('style.load', function(e) {
             layers: ['invalid-osm-areas', 'invalid-osm-areas-problem'],
             radius: 15
         });
-
-        console.log(invalidAreas);
 
         if (invalidAreas) {
 
